72cd874a08b49dbf479dbb00de68c7a5547b742d
appliedFinance <atlusr@yahoo.com>
2018-02-28 18:05:45 -0500

    Initial commit.

---
+++ router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+router.get('/', (req,res) => { res.json(BlogPosts.get()); });
+
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const say = (s) => { console.log(s); };
+const blogPostRouter = require('./router');
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => console.log(`Your app is listening on port ${process.env.PORT || 8080}`));
+
+
+
+
