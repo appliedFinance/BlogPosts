9febc18cd5e79e62c9ebc1d5908d5a3147052864
appliedFinance <atlusr@yahoo.com>
2018-03-01 01:52:52 -0500

    It's talking.

--- router.js
+++ router.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
-router.get('/', (req,res) => { res.json(BlogPosts.get()); });
+const say = (s) => {console.log(s);};
+function makeText(n) {
+	let cc = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	let s = "";
+	while(n>0) 
+	{
+		s += cc[Math.floor(Math.random()*cc.length)];
+		if (n % 5 == 0) { s+= " "; }
+		else if (n % 17 == 0) { s+= ". "; }
+		--n;
+	}
+	s += ".";
+	return s;
+}
+
+BlogPosts.create(
+		"What to do If",
+		makeText(242),
+		"Big Jim");
+BlogPosts.create(
+		"Ok, Then this Happened",
+		makeText(242),
+		"Mr. Jim");
+
+
+router.get("/",  (req,res) => { 
+	res.json( BlogPosts.get() );
+
+});
+
+
+module.exports = router;
+
+
 
 
